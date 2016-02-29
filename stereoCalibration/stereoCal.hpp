5696e70fcc8d959c75fe45b69d1f91033ea7f9e8
Adam Perkins <ap@Adams-MacBook-Pro.local>
2016-02-28 22:18:20 -0500

    Working stereo calibration program.

---
+++ stereoCalibration/stereoCal.hpp
@@ -0,0 +1,14 @@
+//
+//  stereoCal.hpp
+//  stereoVision
+//
+//  Created by Adam Perkins on 2/28/16.
+//  Copyright © 2016 Adam Perkins. All rights reserved.
+//
+
+#ifndef stereoCal_hpp
+#define stereoCal_hpp
+
+#include <stdio.h>
+
+#endif /* stereoCal_hpp */
